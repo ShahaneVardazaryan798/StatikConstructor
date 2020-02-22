773a3b64c0b8a26a28dab02d3686c293f0090d0b
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-02-22 13:31:01 -0800

    Copy constructor

---
+++ CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CopyConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("Shahane", "Erevan");
+            User user1 = new User(user);
+            user.name = "Garik";
+            user.location = "Goris";
+            WriteLine(user.name+","+user.location);
+            WriteLine(user1.name+","+user1.location);
+            ReadLine();
+        }
+    }
+}
---
+++ CopyConstructor/CopyConstructor/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CopyConstructor
+{
+    class User
+    {
+        public string name, location;
+        public User(string a,string b)
+        {
+            name = a;
+            location = b;
+        }
+        public User(User user)//copy constructor
+        {
+            name = user.name;
+            location = user.location;
+        }
+    }
+}
