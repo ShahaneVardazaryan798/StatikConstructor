350b21943ad035525834e3f1eecb53f3d4fb66f3
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-02-22 08:35:00 -0800

    It is an instanse of statik construcor

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            User user1 = new User();
+            ReadLine();
+        }
+    }
+}
---
+++ Constructor/Constructor/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Constructor
+{
+    class User
+    {
+        static User()
+        {
+            WriteLine("Iam static Constructor");
+        }
+        public User()
+        {
+            WriteLine("Iam public Constructor");
+        }
+    }
+}
