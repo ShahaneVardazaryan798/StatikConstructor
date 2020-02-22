46cb2d80556a01602eb1bdb85be08f9449087900
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-02-22 13:57:50 -0800

    Destructor

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Destructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clear();
+            GC.Collect();
+            ReadLine();
+
+                
+        }
+        public static void Clear()
+        {
+            User user = new User();
+                
+        }
+    }
+}
---
+++ Destructor/Destructor/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Destructor
+{
+    class User
+    {
+        public User()
+        {
+            WriteLine("I have Constructor");
+        }
+        ~User()
+        {
+            WriteLine("I have Destructor");
+        }
+    }
+}
