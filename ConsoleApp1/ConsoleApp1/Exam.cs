5c6a07d418f1d3dc48c0c4278659d74dead45293
ShahaneVardazaryan798 <58977401+ShahaneVardazaryan798@users.noreply.github.com>
2020-02-23 03:52:37 -0800

    this

---
+++ ConsoleApp1/ConsoleApp1/Exam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Exam
+    {
+        public static double GetPersentAge(User u)
+        {
+            double i = ((double)470 / 600) * 100;
+            return (i);
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        public string name, location;
+        public long mark = 470;
+        public User(string name,string location)
+        {
+            this.name = name;
+            this.location = location;
+        }
+        public void GetUserDetails()
+        {
+            WriteLine("Name:{0}", name);
+            WriteLine("Location:{0}", location);
+            WriteLine("Mark:{0}", Exam.GetPersentAge(this));
+
+        }
+    }
+}
